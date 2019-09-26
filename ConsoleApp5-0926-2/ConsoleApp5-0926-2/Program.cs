3fa1c810969a4aa4e00fb42b93faa862b001eaed
yapeng hao <yapeng.hao@email.com>
2019-09-26 13:53:25 -0400

    test

---
+++ ConsoleApp5-0926-2/ConsoleApp5-0926-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5_0926_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("master");
+        }
+    }
+}
